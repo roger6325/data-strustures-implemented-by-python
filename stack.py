6376dca8b5c2929efee8ff2d3d60cb2cbaad0454
Roger Chou <dear.roger@gmail.com>
2020-04-01 16:50:53 +0800

    Add the pop function

--- stack.py
+++ stack.py
@@ -1,54 +1,29 @@
 class Stack:
-  def __init__(self, data = None):
-    # create a list that store data
 
-    if data == None:
-      self.stack = list()
-    elif isinstance(data, list):
-      self.stack = data
-    else:
-      raise ValueError('input data should be list')
-    return
+    def __init__(self):
+        self.stack = []
 
-  def __iter__(self):
-    return iter(self.stack)
+    def add(self, dataval):
+    # Use list append method to add element
+        if dataval not in self.stack:
+            self.stack.append(dataval)
+            return True
+        else:
+            return False
 
-  def push(self, data):
-    # put data into the top of stack
+    # Use peek to look at the top of the stack
+    def peek(self):
+	    return self.stack[-1]
+    def pop(self):
+        if len(self.stack)>0:
+            return self.stack.pop()
 
-    self.stack.append(data)
-    return
-
-  def pop(self):
-    # remove the data on the top and return it
-
-    return self.stack.pop()
-
-  def top(self):
-    # get the data value of the top item
-    return self.stack[-1]
-
-  def output_stack(self):
-    # print the full stack
-    
-    data = []
-    for x in self.stack:
-      data.append(x)
-
-    print(data)
-    return
-
-"""
-stack1 = Stack()
-stack1.push(1)
-stack1.push(3)
-stack1.push(2)
-print(stack1.pop())
-stack1.output_stack()
-
-stack2 = Stack([4,6,2,1,6,3])
-stack2.pop()
-stack2.output_stack()
-for x in stack2:
-  print(x)
-"""
+AStack = Stack()
+AStack.add("Mon")
+AStack.add("Tue")
+print(AStack.peek())
+AStack.add("Wed")
+AStack.add("Thu")
+print(AStack.peek())
+print(AStack.pop())
+print(AStack.peek())
